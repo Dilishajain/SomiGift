8ac4859ef7f835dbd0af5aeac1501df051dd276f
Dilishajain <kaladilisha.jain@outlook.com>
2019-05-15 18:20:20 +0530

    first commit

---
+++ somi/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SomiConfig(AppConfig):
+    name = 'somi'
---
+++ somi/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.conf import settings
+from django.utils import timezone
+
+
+# Create your models here.
+class Images(models.Model) :
+	title = models.CharField(max_length=100)
+	photo = models.ImageField(upload_to = 'Documents/',null = True)
+
+	def __str__(self):
+		return self.title
