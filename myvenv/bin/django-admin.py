b43b66239572144f7d9643c1a83e14925afae047
Dilishajain <kaladilisha.jain@outlook.com>
2019-05-16 10:56:26 +0530

    pics displayed

---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/dilisha/Desktop/Projects/Django-1/myvenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
--- somi/models.py
+++ somi/models.py
@@ -6,7 +6,7 @@ from django.utils import timezone
 # Create your models here.
 class Images(models.Model) :
 	title = models.CharField(max_length=100)
-	photo = models.ImageField(upload_to = 'Documents/',null = True)
+	photo = models.ImageField(upload_to = 'static/somi/',null = True)
 
 	def __str__(self):
 		return self.title
